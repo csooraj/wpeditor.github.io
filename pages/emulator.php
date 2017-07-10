447715961b9d52e12b33b1a7ac3fe0c4c4269389
Sooraj C <sooraj@Soorajs-MacBook-Air.local>
2017-07-10 18:15:51 +0530

    Updated appetize io url

--- pages/emulator.php
+++ pages/emulator.php
@@ -1,3 +1,3 @@
 <div class="col-md-6">
-  <iframe style="border:1px solid black" id="mine" onLoad="HandleLoader()" class="embed-responsive-item" src="https://appetize.io/embed/bn5czpzexmjrjde74ddbcfcu5g?device=nexus5&scale=75&orientation=portrait&osVersion=7.0" width="305" height="590" scrolling="no"></iframe>
+  <iframe style="border:1px solid black" id="mine" onLoad="HandleLoader()" class="embed-responsive-item" src="https://appetize.io/embed/h99hx4tgj1dqt9y2c31fpaenx4?device=nexus5&scale=75&orientation=portrait&osVersion=7.0" width="305" height="590" scrolling="no"></iframe>
 </div>
